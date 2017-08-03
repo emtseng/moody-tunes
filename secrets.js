c4485492314910e0d07bb91420582a66c4e26bef
emtseng <etseng42@gmail.com>
2017-08-03 16:41:05 -0400

    updates master

--- secrets.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  spotifyClientId: process.env.SPOTIFY_CLIENT_ID || '5731dd00e04943969cd5e1d6ce78f8ef',
-  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET || '56af518f0b244e0bb1df8e2e27fde838',
-  spotifyRedirectURI: process.env.SPOTIFY_REDIRECT_URI || 'http://localhost:3000/',
-  googleKey: process.env.GOOGLE_KEY || 'AIzaSyBIqWwJFKomePxgKH-CQSUaNMMHQ1RtHCU'
-}
