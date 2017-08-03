de46c5b1d0e65b4660928b1c18b529540618821a
emtseng <etseng42@gmail.com>
2017-08-02 20:31:05 -0400

    sets config vars

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 module.exports = {
-  spotifyClientId: '5731dd00e04943969cd5e1d6ce78f8ef',
-  spotifyClientSecret: '56af518f0b244e0bb1df8e2e27fde838',
-  spotifyRedirectURI: 'http://localhost:3000/',
-  googleKey: 'AIzaSyBIqWwJFKomePxgKH-CQSUaNMMHQ1RtHCU'
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID || '5731dd00e04943969cd5e1d6ce78f8ef',
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET || '56af518f0b244e0bb1df8e2e27fde838',
+  spotifyRedirectURI: process.env.SPOTIFY_REDIRECT_URI || 'http://localhost:3000/',
+  googleKey: process.env.GOOGLE_KEY || 'AIzaSyBIqWwJFKomePxgKH-CQSUaNMMHQ1RtHCU'
 }
